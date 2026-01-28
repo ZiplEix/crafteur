23e1890a929aa9973a5a4669e64f70e6d6329eeb
LEROYER Baptiste <ZiplEix@users.noreply.github.com>
2026-01-28 12:06:35 +0100

    feat: Add documentation menu structure and correct the gitignore path for the data directory.

---
+++ frontpage/src/lib/data/docsParams.ts
@@ -0,0 +1,18 @@
+export const docMenu = [
+    {
+        title: "Démarrage",
+        items: [
+            { title: "Introduction", href: "/docs/intro" },
+            { title: "Installation", href: "/docs/install" },
+            { title: "Architecture", href: "/docs/architecture" }
+        ]
+    },
+    {
+        title: "Guide Administrateur",
+        items: [
+            { title: "Gestion des Serveurs", href: "/docs/manage-servers" },
+            { title: "Mods & Plugins", href: "/docs/addons" },
+            { title: "Tâches Planifiées", href: "/docs/scheduler" }
+        ]
+    }
+];
